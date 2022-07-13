d1e22812eb620af26b1d69a2f458961d31369531
JulianL234 <108952771+JulianL234@users.noreply.github.com>
2022-07-13 18:26:50 +0200

    Update BetriebsstelleController.java

--- db_project/src/main/java/com/dbProject/betriebsstelle/BetriebsstelleController.java
+++ db_project/src/main/java/com/dbProject/betriebsstelle/BetriebsstelleController.java
@@ -1,24 +1,13 @@
 package com.dbProject.betriebsstelle;
 
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
-import com.google.gson.Gson;
 
 
 @RestController
@@ -28,21 +17,22 @@ public class BetriebsstelleController {
 	private BetriebsstelleService bsService = new BetriebsstelleService();
 	Map<String, Betriebsstelle> bsMap = bsService.createMapFromCSV();
 	
-
 	@GetMapping(path="/betriebsstelle/{bsAbk}")
 	@ResponseStatus(code = HttpStatus.OK)
-	public String getBsInfos (@PathVariable("bsAbk") String bsAbk) {
+	public String getBs (@PathVariable("bsAbk") String bsAbk) {
 		
+		// Betriebsstelle gibt es
+		if (bsMap.containsKey(bsAbk.toUpperCase())) {
+			String outputline = "HTTP-STATUS: 200\n\n"
+					+ "{\n\tName: " + bsMap.get(bsAbk.toUpperCase()).getName()
+					+ "\n\tKurzname: " + bsMap.get(bsAbk.toUpperCase()).getKurzname()
+					+ "\n\tTyp: " + bsMap.get(bsAbk.toUpperCase()).getTyp() + "\n}";	
+			return outputline;
+		} 
 		
-		if (bsMap.containsKey(bsAbk)) { // Betriebsstelle gibt es
-			//System.out.println(HttpStatus.OK.is2xxSuccessful());
-			//GJSON oder ähnliches .. TO DO
-			return "Test\n";
-			// to do: HTTP STATUS:200 + JSON Format
-		}
-		
-		else { // Betriebsstelle  gibt es nicht
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Betriebsstelle ist nicht vorhanden!");
+		// Betriebsstelle  gibt es nicht
+		else { // Fehlermeldung - HTTP-Status 400
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
 		}
 	}
 	
